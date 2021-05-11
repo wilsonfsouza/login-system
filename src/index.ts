68a74db2584f347f2f3ec025278e70909befad30
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-10 22:03:49 -0700

    chore: added eslint, prettier, and editorconfig settings.

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: "ok" })
+  return response.json({ message: "ok" })
 })
 
 app.listen(3333, () => {
-    console.log("Server started on port 3333");
+  console.log("Server started on port 3333");
 });
