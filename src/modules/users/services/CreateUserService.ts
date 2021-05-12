242051ac550c809c06836f4606c2d3b859caa25f
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-11 23:09:25 -0700

    feat: added logic to create user.

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,5 +1,20 @@
-import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import { FakeUser as User } from '../infra/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
 
 class CreateUserService {
-  public async execute(data: ICreateUserDTO): Promise<void> { }
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const user = await fakeUsersRepository.create({ name, email, password });
+
+    return user;
+  }
 }
+
+export default CreateUserService;
