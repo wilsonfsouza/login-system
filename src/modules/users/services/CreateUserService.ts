12ab7f296a47de4ea1f6ec01b45f04e96a821cd4
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-11 23:21:53 -0700

    feat: added conditional to check if email is already taken, if yes, it will throw a new error.

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -11,6 +11,12 @@ class CreateUserService {
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const fakeUsersRepository = new FakeUsersRepository();
 
+    const checkUserExists = await fakeUsersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new Error('This email address has been already used.')
+    }
+
     const user = await fakeUsersRepository.create({ name, email, password });
 
     return user;
