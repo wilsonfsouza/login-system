29428fc62369a8af0e53b6c5e39199b4b16464e5
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-12 00:39:56 -0700

    tests: check if response.user is the same as the user authenticated.

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,27 @@
+import AppError from '../../../shared/errors/AppError';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+let authenticateUser: AuthenticateUserService;
+let fakeUsersRepository: FakeUsersRepository;
+
+describe('AuthenticateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    authenticateUser = new AuthenticateUserService(fakeUsersRepository);
+  });
+
+  it('should be able to authenticate a User', async () => {
+    const user = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.user).toEqual(user);
+  });
+})
