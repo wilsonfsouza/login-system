ef5351ede649f75f67cae2ef29b957b3f3da1246
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-11 22:49:13 -0700

    feat: implemented IUsersRepository and create method.

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,6 +1,8 @@
+import { uuid } from 'uuidv4';
 import ICreateUserDTO from '../../dtos/ICreateUserDTO';
 import IUsersRepository from '../IUsersRepository';
 import { User } from '../IUsersRepository';
+import { FakeUser } from '../../infra/entities/User';
 
 export default class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
@@ -13,11 +15,24 @@ export default class FakeUsersRepository implements IUsersRepository {
     return;
   }
 
-  public async create(data: ICreateUserDTO): Promise<User> {
-    return;
+  public async create({
+    name,
+    email,
+    password
+  }: ICreateUserDTO): Promise<User> {
+    const user = new FakeUser({
+      id: uuid(),
+      name,
+      email,
+      password
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
-  public async save(user: User): Promise<User> {
+  public async save(user: User): Promise<User | undefined> {
     return;
   }
 }
