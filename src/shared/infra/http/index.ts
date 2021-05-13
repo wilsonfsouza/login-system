fcef7ee2f3a0c4a847d9515b293380ed43a773d2
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:25:19 -0700

    feat: added celebrate error handling.

--- src/shared/infra/http/index.ts
+++ src/shared/infra/http/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
