7365703494a29734a110b49f3e8ebb2a397076de
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 10:14:59 -0700

    feat: implemented update method, returning user without its password.

--- src/modules/users/infra/http/controllers/UserInfoController.ts
+++ src/modules/users/infra/http/controllers/UserInfoController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import BCryptHashProvider from '../../../providers/HashProvider/implementations/BCryptHashProvider';
 import ShowUserInformationService from '../../../services/ShowUserInformationService';
+import UpdateUserInformationService from '../../../services/UpdateUserInformationService';
 import UsersRepository from '../../customORM/repositories/UsersRepository';
 
 export default class UserInfoController {
@@ -21,8 +23,31 @@ export default class UserInfoController {
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const { name, email, old_password, password } = request.body;
 
+    const usersRepository = new UsersRepository();
+    const hashProvider = new BCryptHashProvider();
 
-    return response.json({ msg: "ok" });
+    const updateUserInformationService = new UpdateUserInformationService(
+      usersRepository,
+      hashProvider
+    );
+
+    const user = await updateUserInformationService.execute({
+      user_id,
+      name,
+      email,
+      old_password,
+      password,
+    });
+
+    const userWithoutPassword = {
+      id: user?.id,
+      name: user?.name,
+      email: user?.email
+    }
+
+    return response.json(userWithoutPassword);
   }
 }
