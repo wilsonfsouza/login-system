ca6a3c4993bac28bfa5ed0725b64afc0d586af7e
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:49:27 -0700

    feat: connected sessions endpoint.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use(usersRouter);
+
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
