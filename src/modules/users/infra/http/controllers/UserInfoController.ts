89fd40e4ad15a15738dc9deec707b0548cf80edd
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 10:06:07 -0700

    feat: implemented get method, returning user without its password.

--- src/modules/users/infra/http/controllers/UserInfoController.ts
+++ src/modules/users/infra/http/controllers/UserInfoController.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express';
+import ShowUserInformationService from '../../../services/ShowUserInformationService';
+import UsersRepository from '../../customORM/repositories/UsersRepository';
 
 export default class UserInfoController {
   public async get(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
 
-    return response.json({ msg: "ok" });
+    const usersRepository = new UsersRepository();
+    const showUserInformationService = new ShowUserInformationService(usersRepository);
+
+    const user = await showUserInformationService.execute({ user_id });
+
+    const userWithoutPassword = {
+      id: user?.id,
+      name: user?.name,
+      email: user?.email
+    }
+
+    return response.json(userWithoutPassword);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
