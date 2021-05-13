f2b87ad821053b27b8078ea7e4473036a2cf11b5
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 00:06:04 -0700

    feat: added input to executed method.

--- src/modules/users/services/UpdateUserInformationService.ts
+++ src/modules/users/services/UpdateUserInformationService.ts
@@ -1,8 +1,14 @@
 
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+}
+
 class UpdateUserInformationService {
   constructor() { }
 
-  public async execute(): Promise<void> {
+  public async execute({ user_id, name, email }: IRequest): Promise<void> {
 
   }
 }
