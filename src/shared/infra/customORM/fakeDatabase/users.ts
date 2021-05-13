ec65aa6da750dd9ae07e4fa6d1ea4dfcb333cd2e
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 07:02:41 -0700

    feat: created fake users database.

---
+++ src/shared/infra/customORM/fakeDatabase/users.ts
@@ -0,0 +1,14 @@
+export let users = [
+  {
+    id: "1bb2f264-406f-4ab2-b674-9bf492dc2380",
+    name: "Wilson Franca",
+    email: "wilson.franca.92@gmail.com",
+    password: "dac9630aec642a428cd73f4be0a03569"
+  },
+  {
+    id: "6a0927e1-1c1a-4555-b63a-a84fba1e88fe",
+    username: "Luiza Costa",
+    email: "joe@provi.com",
+    password: "8cd73f4be0a03569dac9630aec642a42"
+  }
+];
