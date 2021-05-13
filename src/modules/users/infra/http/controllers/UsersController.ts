e9987d449b17bfdba0e09b85b8cd78e003fa7184
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:13:34 -0700

    feat: get user info from request.body.

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
     return response.json({ msg: 'ok' });
   }
 }
