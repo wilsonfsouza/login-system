e9c5d2e358a1f52b8d95db796ad103368e4ba91e
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:56:24 -0700

    feat: instantiated authenticate user service.

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
+import BCryptHashProvider from '../../../providers/HashProvider/implementations/BCryptHashProvider';
 
 import AuthenticateUserService from '../../../services/AuthenticateUserService';
+import UsersRepository from '../../customORM/repositories/UsersRepository';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
+
+    const usersRepository = new UsersRepository();
+    const hashProvider = new BCryptHashProvider();
+
+    const authenticateUserService = new AuthenticateUserService(
+      usersRepository,
+      hashProvider
+    );
 
     return response.json({ msg: 'ok' });
   }
