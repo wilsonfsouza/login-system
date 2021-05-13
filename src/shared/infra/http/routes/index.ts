43bfb83e6bba42e557a4c39bc2095889990be133
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 09:43:46 -0700

    feat: added user info endpoint as profile to simplify url.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
+import userInfoRouter from '../../../../modules/users/infra/http/routes/userInfo.routes';
 
 const routes = Router();
 
@@ -9,5 +10,6 @@ routes.use(usersRouter);
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/profile', userInfoRouter);
 
 export default routes;
