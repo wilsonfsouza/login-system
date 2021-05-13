8245439e4558927c2f26f07f9cc3ca604db0d53c
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 10:05:21 -0700

    feat: added middleware to all routes to verify if token is valid and user is authenticated.

--- src/modules/users/infra/http/routes/userInfo.routes.ts
+++ src/modules/users/infra/http/routes/userInfo.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import ensureUserAuthenticated from '../../middlewares/ensureUserAuthenticated';
 
 import UserInfoController from '../controllers/UserInfoController';
 
 const userInfoRouter = Router();
 const userInfoController = new UserInfoController();
+
+userInfoRouter.use(ensureUserAuthenticated);
 
 userInfoRouter.get(
   '/',
