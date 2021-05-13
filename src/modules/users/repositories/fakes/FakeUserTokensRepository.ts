d4b2ec6d9294047605059697b459417754cc66a3
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-12 20:46:22 -0700

    feat: base class implementing interface with empty methods.

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,14 @@
+import { FakeUserToken as UserToken } from "../../infra/entities/UserToken";
+import IUserTokensRepository from "../IUserTokensRepository";
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    return;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    return;
+  }
+}
