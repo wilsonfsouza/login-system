6cffc8899313e89433eb1cc9388b0b763ac816a3
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:17:34 -0700

    feat: instantiated create user service.

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express';
 
+import BCryptHashProvider from '../../../providers/HashProvider/implementations/BCryptHashProvider';
+import CreateUserService from '../../../services/CreateUserService';
+import UsersRepository from '../../customORM/repositories/UsersRepository';
+
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body;
+
+    const hashProvider = new BCryptHashProvider();
+    const usersRepository = new UsersRepository();
+
+    const createUserService = new CreateUserService(
+      usersRepository,
+      hashProvider
+    );
+
     return response.json({ msg: 'ok' });
   }
 }
