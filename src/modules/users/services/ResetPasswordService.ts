71ab5f920505b8ae542a93ae08390d656a14f96b
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-12 21:31:52 -0700

    feat: base class with steps to be taken.

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,12 @@
+class ResetPasswordService {
+  constructor() { }
+
+  public async execute(): Promise<void> {
+    // Check if user has a valid jwt token
+    // Get user from userToken.id
+    // Get when the token was created
+    // Check if token has expired
+    // Rewrite Hashed user password
+    // Update user information
+  }
+}
