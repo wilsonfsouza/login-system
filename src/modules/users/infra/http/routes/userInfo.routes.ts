2258b53901894c1ea85e483148eb902f9746735a
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 10:16:51 -0700

    feat: added body input validation with an additional field of password_confirmation that must match password.

--- src/modules/users/infra/http/routes/userInfo.routes.ts
+++ src/modules/users/infra/http/routes/userInfo.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureUserAuthenticated from '../../middlewares/ensureUserAuthenticated';
 
 import UserInfoController from '../controllers/UserInfoController';
@@ -14,6 +15,15 @@ userInfoRouter.get(
 
 userInfoRouter.put(
   '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
   userInfoController.update);
 
 export default userInfoRouter;
