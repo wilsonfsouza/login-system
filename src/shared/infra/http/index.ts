389fdf12fcb8701352cf838bdcf71fc95b0d8b02
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 09:31:41 -0700

    feat: added custom error message when error is an instance of AppError.

--- src/shared/infra/http/index.ts
+++ src/shared/infra/http/index.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import { errors } from 'celebrate';
 import routes from './routes';
+import AppError from '../../errors/AppError';
 
 const app = express();
 
@@ -9,6 +10,21 @@ app.use(express.json());
 
 app.use(routes);
 app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
