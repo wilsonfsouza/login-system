8f304bae0b5a3bb12b255476a0c8b56637e89bef
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 07:49:33 -0700

    feat: added routes to main server file.

--- src/shared/infra/http/index.ts
+++ src/shared/infra/http/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: "ok" })
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
