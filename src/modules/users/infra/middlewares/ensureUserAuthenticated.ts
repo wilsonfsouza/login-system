d09c8ac3d342cefba8f7ff7491133c48b123e26e
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 09:53:40 -0700

    feat: created middleware to check if user is authenticated using the jwt token.

---
+++ src/modules/users/infra/middlewares/ensureUserAuthenticated.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authenticationConfig from '../../../../config/authentication';
+import AppError from '../../../../shared/errors/AppError';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureUserAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing.', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authenticationConfig.jwt.secret);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = { id: sub };
+
+    return next();
+  } catch (err) {
+    throw new AppError('Invalid JWT Token.', 401);
+  }
+}
