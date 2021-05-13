bcd3c5d520163975acb0adff60b2ab5433a99757
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:54:17 -0700

    feat: got user input from request body.

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -4,6 +4,7 @@ import AuthenticateUserService from '../../../services/AuthenticateUserService';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
 
     return response.json({ msg: 'ok' });
   }
