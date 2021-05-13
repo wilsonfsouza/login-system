3bc321bfd10d611b5fdb7a010a9d73b3e4d59841
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-13 08:40:00 -0700

    feat: added users endpoint.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
+import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
+routes.use(usersRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
